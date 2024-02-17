5f2bfca944a521bbb903bfe063e7bc723c741702
bodrovdev <bodrovigor02@gmail.com>
2024-02-18 00:59:03 +0300

    refactor: 💡 initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./scss/style.scss";
+
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <main className="page-content">
+      </main >
+    </>
+  );
+}
+
+export default App; 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../scss/style.scss';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={`${styles.root} page-header`}>
+      <div className={`${styles.root__container} base-container`}>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
